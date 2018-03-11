1a211035c823b8d433230332680877fe8ea11e4c
Carl <fonts@spoturdeal.com>
2018-03-11 17:02:43 +0100

    Added devices that are on

--- MMM-Domoticz.js
+++ MMM-Domoticz.js
@@ -9,12 +9,12 @@
         updateInterval: 45,                          // every 45 seconds
         apiBase: '192.168.xx.xxx',                   // the IPaddress of you Domoticz HC in your home network
         apiPort: 8088,                                 // just leave at 80
-        apiEndpoint: 'json.htm?type=devices&filter=temp&used=true&order=Name',   // access to api
         moduleTitle: "Current temperatures Domoticz",  // You can adapt the following text to fit your language
         energyTitle: "Energy use",                   // The tile for the energy use part
         energyNow: "Currently",                      // Label to show current use
         energyTotal: "Total used",                   // Label for total registred energy used
-        showItems: ['temperature']          // Currently available temperature, energy     
+        showItems: ['temperature','energy'],          // Currently available temperature, energy
+        excludeDevices: ['none']              // Devices you don`t want to see
 	},
 	start: function() {
 		Log.info('Starting module: ' + this.name);
@@ -27,29 +27,44 @@
 	update: function(){
 		this.sendSocketNotification(
 			'DOMOTICZ_READ',
-			'http://' + this.config.apiUser + ':' + this.config.apiPw + '@' + this.config.apiBase + ":" + this.config.apiPort + "/" + this.config.apiEndpoint);
+			'http://' + this.config.apiUser + ':' + this.config.apiPw + '@' + this.config.apiBase + ":" + this.config.apiPort + "/" + 'json.htm?type=devices&used=true&order=Name');
 	},
 	render: function(data){
-		var text = '<div>';
+
+    var text = '<div>';
         var therm ='<header class="module-header">' + this.config.moduleTitle + '</header><table>';
+        var power='<header class="module-header">' + this.config.energyTitle + '</header><table>';
         var powerUse=0;
         var usedEnergy=0;
+        var powerCount=0;
+        var tempCount=0;
         for (i=0;i<data.result.length;i++){
             var dev=data.result[i];
-            
-               therm += '<tr><td class="small">' + dev.Name + '</td><td class="small '+(dev.Temp< 0.6?'red':'')+'">' + parseFloat(dev.Temp).toFixed(1) + "&deg;</td></tr>";   
-            
-        }           
+            if (this.config.excludeDevices.indexOf(dev.Name) == -1) {
+              if (dev.Type.indexOf('Temp') >- 1){
+                tempCount++;
+                therm += '<tr><td class="small">' + dev.Name + '</td><td class="small '+(dev.Temp< 0.6?'red':'')+'">' + parseFloat(dev.Temp).toFixed(1) + "&deg;</td></tr>";
+              } else if (dev.Data == "On") {
+                powerCount++
+                power += '<tr><td class="small">' + dev.Name + '</td><td class="small ">' + (dev.Data) + "</td></tr>";
+              }
+            }
+
+
+        }
         therm += '</table>';
-        
-        //var power='<header class="module-header">' + this.config.energyTitle + '</header>';
+        power += '</table>';
+
         //power +='<table><tr><td class="small">' + this.config.energyNow + '</td><td class="small">' + powerUse.toFixed(2) +' Watt</td></tr>';
         //power +='<tr><td class="small">' + this.config.energyTotal +'</td><td class="small">' + usedEnergy.toFixed(2) +' kWh</td></tr></table>';
-        
-        text += (this.config.showItems.indexOf('temperature') !== -1?therm:'');
-        //text += (this.config.showItems.indexOf('energy') !== -1?power:''); 
+        if (tempCount >0 ){
+            text += (this.config.showItems.indexOf('temperature') !== -1?therm:'');
+        }
+        if (powerCount > 0){
+            text += (this.config.showItems.indexOf('energy') !== -1?power:'');
+        }
         text += '</div>';
-        
+
 		this.loaded = true;
 		// only update dom if content changed
 		if(this.dom !== text){
@@ -59,7 +74,7 @@
 	},
 	html: {
 		loading: '<div class="dimmed light small">Loading Domoticz data ....</div>'
-		
+
 	},
 	getScripts: function() {
 		return [

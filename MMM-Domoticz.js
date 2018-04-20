cc18126ae24f249510206029e7efd6addd1dc4b9
Carl <fonts@spoturdeal.com>
2018-04-20 19:11:12 +0200

    Add files via upload

--- MMM-Domoticz.js
+++ MMM-Domoticz.js
@@ -1,6 +1,6 @@
 /* Magic Mirror
  * Module: MagicMirror-Domoticz-Module
- * version 1.04 15th April 2018
+ * version 1.05 20th April 2018
  * By SpoturDeal https://github.com/SpoturDeal
  * MIT Licensed.
  */
@@ -8,8 +8,9 @@
 	defaults: {
         updateInterval: 45,                          // every 45 seconds
         apiBase: '192.168.xx.xxx',                   // the IPaddress of you Domoticz HC in your home network
-        apiPort: 8080,                                 // just leave at 80
-        moduleTitle: "Current temperatures Domoticz",  // You can adapt the following text to fit your language
+        apiPort: 8080,                               // just leave at 80
+        moduleTitle: "My smart home",                //
+        temperatureTitle:"Current temperatures Domoticz",  // You can adapt the following text to fit your language
         energyTitle: "Energy used by",                 // The tile for the energy use part
         batteryTitle: "Battery level",
         blindsTitle:  "Blinds",
@@ -39,101 +40,98 @@
 
     var text = '<div>';
     var therm = ""; power = ""; batt = ""; co = ""; blinds = ""; humi="";
-        if (this.config.subMenus === true) {
-           var therm ='<header class="module-header">' + this.config.moduleTitle + '</header><table>';
-           var power='<header class="module-header">' + this.config.energyTitle + '</header><table>';
-           var batt ='<header class="module-header">' + this.config.batteryTitle + '</header><table>';
-           var co ='<header class="module-header">' + this.config.coTitle + '</header><table>';
-           var blinds ='<header class="module-header">' + this.config.blindsTitle + '</header><table>';
-        } else {
-           text += '<header class="module-header">Domoticz</header><table>';
-        }
-        var powerUse=0; usedEnergy=0;
-        var powerCount=0; tempCount=0; coCount=0; batteryCount=0;blindsCount=0;
-        for (i=0;i<data.result.length;i++){
-            var dev=data.result[i];
-            if (this.config.excludeDevices.indexOf(dev.Name) == -1) {
-              if (dev.Type.indexOf('Temp') >- 1){
-                tempCount++;
-                therm += '<tr><td class="small">' + dev.Name  +'</td><td class="small '+(dev.Temp< 0.6?'red':'')+'">' + parseFloat(dev.Temp).toFixed(1) + '&deg; <i class="fa fa-thermometer-half"></i></td></tr>';
-              } else if (dev.Data == "On" || dev.Data == "Set Level") {
-                powerCount++
-                cImage = parseInt(dev.CustomImage);
-                switch (cImage) {
-                  case 1:
+    if (this.config.subMenus === true) {
+       var therm ='<header class="module-header">' + this.config.temperatureTitle + '</header><table>';
+       var power='<header class="module-header">' + this.config.energyTitle + '</header><table>';
+       var batt ='<header class="module-header">' + this.config.batteryTitle + '</header><table>';
+       var co ='<header class="module-header">' + this.config.coTitle + '</header><table>';
+       var blinds ='<header class="module-header">' + this.config.moduleTitle + '</header><table>';
+    } else {
+       text += '<header class="module-header">' + this.config.moduleTitle + '</header><table>';
+    }
+    var powerUse=0; usedEnergy=0;
+    var powerCount=0; tempCount=0; coCount=0; batteryCount=0;blindsCount=0;
+    for (i=0;i<data.result.length;i++){
+        var dev=data.result[i];
+        if (this.config.excludeDevices.indexOf(dev.Name) == -1) {
+           if (dev.Type.indexOf('Temp') >- 1){
+              tempCount++;
+              therm += '<tr><td class="small">' + dev.Name  +'</td><td class="small ';
+              therm += (dev.Temp< 0.6?'red':'')+'">' + parseFloat(dev.Temp).toFixed(1);
+              therm += '&deg; <i class="fa fa-thermometer-half"></i></td></tr>';
+           } else if (dev.Data == "On" || dev.Data == "Set Level") {
+              powerCount++
+              cImage = parseInt(dev.CustomImage);
+              switch (cImage) {
+                case 1:
                     icon="fa-plug"
                     break;
-                  case 2:
+                case 2:
                     icon="fa-television"
                     break;
-                  case 9:
+                case 9:
                     icon="fa-power-off"
                     break;
-                  case 17:
+                case 17:
                     icon="fa-desktop"
                     break;
-                  default:
+                default:
                     icon="fa-lightbulb-o"
-                }
-                power += '<tr><td class="small">' + dev.Name + '</td><td class="small "><i class="fa ' + icon + '"></i></td></tr>';
               }
-              if (dev.SwitchType == "Blinds" || dev.SwitchType == "Blinds Inverted"){
-                blindsCount++;
-                blinds += '<tr><td class="small">' + dev.Name  +'</td><td class="small '+(dev.Status=="Closed"?'yellow':'')+'"><i class="fa fa-arrow-' + (dev.Status=="Closed"?'down':'up') + '"></i></td></tr>';
+              power += '<tr><td class="small">' + dev.Name + '</td><td class="small "><i class="fa ' + icon + '"></i></td></tr>';
+           }
+          if (dev.SwitchType == "Blinds" || dev.SwitchType == "Blinds Inverted"){
+              blindsCount++;
+              blinds += '<tr><td class="small">' + dev.Name  +'</td><td class="small ';
+              blinds += (dev.Status=="Closed"?'yellow':'')+'"><i class="fa fa-arrow-';
+              blinds += (dev.Status=="Closed"?'down':'up') + '"></i></td></tr>';
+          }
+          if (dev.BatteryLevel <= this.config.batteryThreshold) {
+              batteryCount++;
+              batteryIcon = "fa-battery-half"
+              if (dev.BatteryLevel < 15){
+                  batteryIcon = "fa-battery-quarter"
               }
-              if (dev.BatteryLevel <= this.config.batteryThreshold) {
-                 batteryCount++;
-                 batteryIcon = "fa-battery-half"
-                 if (dev.BatteryLevel < 15){
-                    batteryIcon = "fa-battery-quarter"
-                 }
-                 batt += '<tr><td class="small '+(dev.BatteryLevel< 15?'red':'')+'">' + dev.Name  +'</td><td class="small '+(dev.BatteryLevel< 15?'red':'')+'"><i class="fa ' + batteryIcon + '"></i> ' + dev.BatteryLevel + "%</td></tr>";
+              batt += '<tr><td class="small '+(dev.BatteryLevel< 15?'red':'')+'">' + dev.Name;
+              batt +='</td><td class="small '+(dev.BatteryLevel< 15?'red':'');
+              bat t+='"><i class="fa ' + batteryIcon + '"></i> ' + dev.BatteryLevel + "%</td></tr>";
+          }
+          if (dev.Type == "Air Quality"){
+              pts=dev.Data.split(' ');
+              if (pts[0] > this.config.coThreshold){
+                 coCount++;
+                 alarmLvl=this.config.coThreshold + 200;
+                 co += '<tr><td class="small">' + dev.Name  +'</td><td class="small '+(pts[0] > alarmLvl?'red':'')+'">' + dev.Data + '</td></tr>';
               }
-              if (dev.Type == "Air Quality"){
-                pts=dev.Data.split(' ');
-                if (pts[0] > this.config.coThreshold){
-                   coCount++;
-                   alarmLvl=this.config.coThreshold + 200;
-                   co += '<tr><td class="small">' + dev.Name  +'</td><td class="small '+(pts[0] > alarmLvl?'red':'')+'">' + dev.Data + '</td></tr>';
-                }
-              }
-              if (dev.Type.indexOf('Humidity') >- 1){
-                tempCount++;
-                humi += '<tr><td class="small">' + dev.Name  +'</td><td class="small">' + parseInt(dev.Humidity) + '% <i class="fa fa-tint"></i></td></tr>';
-              }
-            }
+          }
+          if (dev.Type.indexOf('Humidity') >- 1){
+              tempCount++;
+              humi += '<tr><td class="small">' + dev.Name  +'</td><td class="small">';
+              humi += parseInt(dev.Humidity) + '% <i class="fa fa-tint"></i></td></tr>';
+          }
+      }
 
 
-        }
-        therm += humi;
-        if (this.config.subMenus === true) {
-           therm += '</table>';
-           power += '</table>';
-           batt += '</table>';
-           blinds +='</table>';
-           co += '</table>';
-        }
-        //power +='<table><tr><td class="small">' + this.config.energyNow + '</td><td class="small">' + powerUse.toFixed(2) +' Watt</td></tr>';
-        //power +='<tr><td class="small">' + this.config.energyTotal +'</td><td class="small">' + usedEnergy.toFixed(2) +' kWh</td></tr></table>';
-        if (tempCount >0 ){
-            text += (this.config.showItems.indexOf('temperature') !== -1?therm:'');
-        }
-        if (powerCount > 0){
-            text += (this.config.showItems.indexOf('energy') !== -1?power:'');
-        }
-        if (batteryCount > 0){
-            text += (this.config.showItems.indexOf('battery') !== -1?batt:'');
-        }
-        if (blindsCount > 0){
-            text += (this.config.showItems.indexOf('blinds') !== -1?blinds:'');
-        }
-        if (coCount > 0){
-            text += (this.config.showItems.indexOf('co') !== -1?co:'');
-        }
-        if (this.config.subMenus !== true) {
-            text +='</table>';
-        }
-        text += '</div>';
+    }
+    therm += humi;
+    if (this.config.subMenus === true) {
+       therm += '</table>';
+       power += '</table>';
+       batt += '</table>';
+       blinds +='</table>';
+       co += '</table>';
+    }
+    //power +='<table><tr><td class="small">' + this.config.energyNow + '</td><td class="small">' + powerUse.toFixed(2) +' Watt</td></tr>';
+    //power +='<tr><td class="small">' + this.config.energyTotal +'</td><td class="small">' + usedEnergy.toFixed(2) +' kWh</td></tr></table>';
+    if (tempCount >0 ){    text += (this.config.showItems.indexOf('temperature') !== -1?therm:''); }
+    if (powerCount > 0){   text += (this.config.showItems.indexOf('energy') !== -1?power:''); }
+    if (batteryCount > 0){ text += (this.config.showItems.indexOf('battery') !== -1?batt:''); }
+    if (blindsCount > 0){  text += (this.config.showItems.indexOf('blinds') !== -1?blinds:'');  }
+    if (coCount > 0){      text += (this.config.showItems.indexOf('co') !== -1?co:'');  }
+    if (this.config.subMenus !== true) {
+        text +='</table>';
+    }
+    text += '</div>';
 
 		this.loaded = true;
 		// only update dom if content changed
